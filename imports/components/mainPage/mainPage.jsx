14bd63c59869170594f6a3b3dbd355ea6c6e95c2
kacapka <wojciech.urbansk@gmail.com>
2018-07-26 22:17:23 +0200

    mongo collections init

--- imports/components/mainPage/mainPage.jsx
+++ imports/components/mainPage/mainPage.jsx
@@ -1,17 +1,44 @@
 import React, {Component} from 'react';
 import './mainPage.scss';
-//import bgImage from '/public/main_bg.jpg';
+import {withTracker} from 'meteor/react-meteor-data';
+import {Products} from '/lib/collections';
+import {Meteor} from 'meteor/meteor';
 
 class MainPage extends Component {
+    
+    onBtnClick() {
+        console.log('hey');
+        console.log(Meteor);
+        Meteor.call('addProduct', 'kacapka', err => {
+            if(err) {
+                console.log(err);
+            }    
+        });
+    }
 
     render() {
+        console.log(this.props);
+        console.log(Products);
         return (
             <div id='mainPage'>
-                <div id='mainHero' style={{backgroundImage: "url('/main_bg.jpg')"}} />
+               <button onClick={() => this.onBtnClick()}>klik</button>  
             </div>
         );
     }
 
 }
 
-export default MainPage;
+export default withTracker(() => {
+    let products = [];
+    const handle = Meteor.subscribe('products.public');
+    const handleReady = handle.ready();
+    console.log(handleReady);
+    if(handleReady) {
+        products = Products.find({}).fetch();
+    }
+    
+    return {
+        handleReady,
+        products
+    }
+})(MainPage);
--- server/main.js
+++ server/main.js
@@ -1,4 +1,6 @@
 import { Meteor } from 'meteor/meteor';
+import './methods/Products/insertProduct';
+import './publish/publish_products';
 
 Meteor.startup(() => {
   // code to run on server at startup
