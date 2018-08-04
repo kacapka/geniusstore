a7fc56925e24d9b4acdd37bb69da5e17bb4624f8
kacapka <wojciech.urbansk@gmail.com>
2018-08-04 20:58:06 +0200

    footer form validation

--- imports/components/footer/footerForm/footerForm.jsx
+++ imports/components/footer/footerForm/footerForm.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './footerForm.scss';
+import emailValidation from '../../../functions/emailValidation';
 
 class FooterForm extends Component {
 
@@ -8,13 +9,59 @@ class FooterForm extends Component {
         this.state ={
             name: '',
             email: '',
-            text: ''
-        }
+            text: '',
+            nameError: '',
+            emailError: '',
+            textError: ''
+        };
         this.onInputChange = this.onInputChange.bind(this);
+        this.onSendEmailClick = this.onSendEmailClick.bind(this);
     }
 
     onInputChange(e) {
-        this.setState({[e.target.name]: e.target.value});
+        this.setState({
+            [e.target.name]: e.target.value,
+            [`${e.target.name}Error`]: ''
+        });
+    }
+
+    validateForm() {
+        let validEmail = true;
+        let validName = true;
+        let validText = true;
+        const {name, email, text} = this.state;
+        if(name.length < 2) {
+            this.setState({nameError: 'błedne imię'});
+            validName = false;
+        }
+        if(text.length < 10) {
+            this.setState({nameText: 'zbyt krótka wiadomość'});
+            validText = false;
+        }
+        if(!emailValidation(email)) {
+            this.setState({emailError: 'błędny adres email'});
+            validEmail = false;
+        }
+
+        return validEmail && validName && validText;
+    }
+
+    resetInputs() {
+        this.setState({
+            name: '',
+            email: '',
+            text: '',
+            nameError: '',
+            emailError: '',
+            textError: ''
+        });
+    }
+
+    onSendEmailClick() {
+        if(this.validateForm()) {
+            this.resetInputs();
+            console.log('send email');
+        }
     }
 
     render() {
@@ -30,6 +77,7 @@ class FooterForm extends Component {
                                type='text'
                                onChange={this.onInputChange}
                         />
+                        {this.state.nameError && <p className='input-error'>{this.state.nameError}</p>}
                     </div>
                     <div className='footer-input-wrapper'>
                         <label className='footer-label'>email</label>
@@ -53,7 +101,7 @@ class FooterForm extends Component {
                         />
                     </div>
                     <div id='footerFormSubmit'>
-                        <div id='footerBtnSubmit'>
+                        <div id='footerBtnSubmit' onClick={this.onSendEmailClick}>
                             wyslij
                         </div>
                     </div>
